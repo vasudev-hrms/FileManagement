4d0d443ea5cecc19dcb0e773fc45d8dc6a423cc0
Hosalli <vasudev-hrms@users.noreply.github.com>
2016-06-29 13:35:52 +0530

    test

---
+++ GitHub/MyTestFile.php
@@ -0,0 +1,13 @@
+//Php Content goes here
+
+<?php
+
+   abstract class Staff
+   {
+      abstract function hire();
+      abstract function fire();
+      abstract function promote();
+      abstract function demote();
+   }
+
+?>
